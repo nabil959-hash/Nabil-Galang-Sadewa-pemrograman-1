97e66bb103f1709a082dae654e98ec8f7eea3d6b
nabil959-hash <nabilgalang53@gmail.com>
2026-05-05 21:26:20 +0700

    Add files via upload

---
+++ p1_Nabil/src/Latihan/P1_Latihan.java
@@ -0,0 +1,13 @@
+
+package Latihan;
+
+/**
+ *
+ * @author agun
+ */
+public class P1_Latihan {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
