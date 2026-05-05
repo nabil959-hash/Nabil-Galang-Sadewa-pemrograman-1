2cd5af0eaff3670f1882eadef7c5bd843bc8f9da
nabil959-hash <nabilgalang53@gmail.com>
2026-05-05 21:01:13 +0700

    Add files via upload

---
+++ p7_Nabil/src/Latihan/P7_Latihan.java
@@ -0,0 +1,27 @@
+package Latihan;
+import java.util.Scanner;
+public class P7_Latihan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Input Piliahan : ");
+        int Pilih = input.nextInt();
+        input.nextLine();
+        
+        if(Pilih == 1){
+            System.out.println("Masukan Nama : ");
+            String nama = input.nextLine();
+            
+            System.out.println("Masukan Jurusan");
+            String Jurusan = input.nextLine();
+            System.out.println("Nama : " + nama);
+            System.out.println("Jurusan : " + Jurusan);
+            }else {
+            System.out.println("Pilihan Mu Adalah : " + Pilih);
+            }
+            System.out.println("System Selsesai.");
+        input.close();
+    }
+    
+    
+}
---
+++ p8_Nabil/src/Latihan/Do.java
@@ -0,0 +1,13 @@
+package Latihan;
+
+
+public class Do {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        do{
+            System.out.println("perulangan ke" + i);
+            i++;
+        }while (i <= 5);
+    }
+}
